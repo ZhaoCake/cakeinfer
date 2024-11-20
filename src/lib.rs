17d6c61c345e899f861256d2db14dc36276b6e07
zhaocake <zhaocake@foxmail.com>
2024-11-20 22:53:06 +0800

    Suspend.

--- src/lib.rs
+++ src/lib.rs
@@ -2,3 +2,21 @@ pub struct Tensor {
     pub data: Vec<f32>,
     pub shape: Vec<usize>,
 }
+
+impl Tensor {
+    pub fn new(data: Vec<f32>, shape: Vec<usize>) -> Self {
+        assert_eq!(data.len(), shape.iter().product());
+        Self { data, shape }
+    }
+
+    pub fn add(&self, other: &Tensor) -> Tensor {
+        assert_eq!(self.shape, other.shape);
+        let data = self
+            .data
+            .iter()
+            .zip(other.data.iter())
+            .map(|(a, b)| a + b)
+            .collect();
+        Tensor::new(data, self.shape.clone())
+    }
+}
